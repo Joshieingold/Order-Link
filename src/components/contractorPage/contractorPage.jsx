dfd1d251db5223c3695a73b1878e567547d93131
joshlynch2001 <joshlynch2001@gmail.com>
2025-05-22 18:44:43 +0000

    ctr-page

--- src/components/contractorPage/contractorPage.jsx
+++ src/components/contractorPage/contractorPage.jsx
@@ -1,10 +1,11 @@
 import "./contractorPage.css"
+import {CtrNav} from "./components/ctrNav/ctrNav.jsx"
 import {Navbar} from "../General/navbar/navbar"
 export const CtrPage = () => {
 return (
-    <div>
+    <div className="ctr-page-main">
         <Navbar/>
-        
+        <CtrNav/>
     </div>
 )
 };
